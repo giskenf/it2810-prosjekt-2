09845e960d66055456760014b2a40c52b51cc63a
Giske Naper Freberg <giskenf@stud.ntnu.no>
2020-09-17 19:36:28 +0200

    Revert "Revert "Merge branch 'dev4' into 'giskes-branch'""
    This reverts commit a1bbb29151102f9846eaccf9624074dcbbbe63d8

---
+++ my-app/src/components/TabsBar.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useContext} from "react";
+import './TabsBar.css';
+import {Art} from './Art';
+
+
+export function TabsBar(props: any){
+    const [selectedOption, setSelectOption] = useState("image2")
+
+    function handleClick(e: any){
+        setSelectOption(e.target.value);
+    }
+    console.log(selectedOption)
+
+    return(
+        <>
+        <button className="NavButton" onChange={(e) =>
+            handleClick(e)} value="image1" >1</button>
+        <button className="NavButton"  onChange={(e) =>
+            handleClick(e)} value="image2" >2</button>
+        <button className="NavButton" onChange={(e) =>
+            handleClick(e)} value="image3" >3</button>
+        <button className="NavButton" onChange={(e) =>
+                handleClick(e)} value="image4" >4</button>
+        <button className="NavButton" onChange={(e) =>
+                handleClick(e)} value="image5" >5</button>
+        </>
+        )
+
+}
+/*<button id = "Art2" className="NavButton" >2</button>
+    <button id = "Art3" className="NavButton" >3</button>
+    <button id = "Art4" className="NavButton" >4</button>
+    <button id = "Art5" className="NavButton" >4</button>*/
+
