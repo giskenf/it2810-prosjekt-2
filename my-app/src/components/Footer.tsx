2fcdf5f5717e82a15bf3ce8488e42d528a924f04
giskenf <giskefreberg@hotmail.com>
2020-09-28 09:16:32 +0200

    Fix merge conflicts

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,28 +1,33 @@
 import React, {useContext, useState} from 'react';
 import './Footer.css'
-import {RadioButton} from './RadioButton';
-import {SessionStore} from './SessionStore';
+import {ThemeButton} from './ThemeButton'
+import {SongButton} from "./SongButton";
+import {SessionStore} from "./SessionStore";
+//import {Button} from "./Button"
 import {GlobalContext} from "./GlobalProvider";
 
+interface footerProps{
+    changePoem:(a: number)=>void;
+}
 
-
-
-
-export const Footer: React.FC = () => {
+export const Footer: React.FC<footerProps> = (props: footerProps) => {
     const {themeProvider} = useContext(GlobalContext)
 
     return (
         <>
             <div className = "BackgroundFooter" id={themeProvider.selectedTheme}>
-                <div className = "NavArea">
-                    <div className="break"></div>
-                    <nav className="Nav">
-                        <RadioButton/>
-                        <SessionStore/>
-                    </nav>
+                <div >Choose theme
+                    <ThemeButton/>
                 </div>
-                <div>TEST1</div>
-                <div>TEST2</div>
+                <div>Choose song
+                    <SongButton/>
+                </div>
+                <div>Choose poem
+                    <Button changePoem={props.changePoem}/>
+                </div>
+                <div>Choose kombo
+                    <SessionStore/>
+                </div>
             </div>
         </>
     );
