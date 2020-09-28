8c7f7cdff77957a9d80b8bece013b07b0a989a29
giskenf <giskefreberg@hotmail.com>
2020-09-28 08:58:36 +0200

    Fix merge conflicts

--- my-app/src/components/GlobalProvider.tsx
+++ my-app/src/components/GlobalProvider.tsx
@@ -2,23 +2,25 @@ import React, {useState, createContext, useContext, ReactNode, useMemo} from 're
 
 export const GlobalContext = createContext<any>(1)
 
+
 export const GlobalProvider = (props: any) =>{
 
-    const [selectedOption, setSelectedOption] = useState(1)
+    const [selectedOption, setSelectedOption] = useState(Math.floor(Math.random()*5)+1)
     const [selectedTheme, setSelectedTheme] = useState(1)
+    const [selectedSong,setSelectedSong] = useState("Song")
 
     const buttonProvider = useMemo(() => ({
         selectedOption,setSelectedOption}),[selectedOption,setSelectedOption]);
     const themeProvider = useMemo(() => ({
         selectedTheme,setSelectedTheme}),[selectedTheme,setSelectedTheme]);
+    const songProvider = useMemo(() => ({
+        selectedSong,setSelectedSong}),[selectedSong,setSelectedSong]);
 
     return(
-        <GlobalContext.Provider value={{buttonProvider,themeProvider}}> {props.children} </GlobalContext.Provider>
+        <GlobalContext.Provider value={{buttonProvider,themeProvider,songProvider}}> {props.children} </GlobalContext.Provider>
     )
 
 }
-
-
 
 
 
--- my-app/src/components/SessionStore.tsx
+++ my-app/src/components/SessionStore.tsx
@@ -4,25 +4,28 @@ import {GlobalContext} from "./GlobalProvider";
 
 export function SessionStore(props: any){
     const {themeProvider} = useContext(GlobalContext);
-    //const {songProvider} = useContext(GlobalContext);  /Må gjøre det samme bare for songProvider
+    const {songProvider} = useContext(GlobalContext);
     const [value, setValue] = useState(false);
     const [value2, setValue2] = useState(false);
 
     if (value){
         //console.log(themeProvider.selectedTheme);
-        if(themeProvider.selectedTheme == 1){
+        if(themeProvider.selectedTheme == 1 || songProvider == "Song"){
             window.alert("Du har ikke valgt et tema!");
         }
         sessionStorage.setItem('theme' , themeProvider.selectedTheme);
+        sessionStorage.setItem('song', songProvider.selectedSong);
         setValue(false);
     }
 
     if (value2){
         const theme = sessionStorage.getItem('theme');
-        if(theme == '1'){
+        const song = sessionStorage.getItem('song');
+        if(theme == '1' || song=='Song'){
             window.alert("Du har ikke valgt en kombinasjon!");
         }
         themeProvider.setSelectedTheme(theme);
+        songProvider.setSelectedTheme(song);
         setValue2(false);
     }
 
