635bb6f63e14caf7cf8d0d23d13d7d5cb10f69c2
giskenf <giskefreberg@hotmail.com>
2020-09-28 09:25:00 +0200

    Fix merge conflicts

---
+++ my-app/src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React, {useContext, useState} from "react";
+import {GlobalContext} from "./GlobalProvider";
+
+interface ButtonProps{
+    changePoem:(a: number)=>void;
+}
+export function Button(props: ButtonProps){
+
+    const {songProvider} = useContext(GlobalContext)
+
+    return (
+        <div className="radioButtonsContainer">
+            <div className="radioButtons">
+                <div>
+                    <input type="radio" onChange={() => props.changePoem(2)}/>
+                    Electronica
+                </div>
+                <div>
+                    <input type="radio" onChange={() => props.changePoem(5)}/>
+                    Piano
+                </div>
+                <div>
+                    <input type="radio" onChange={() => songProvider.setSelectedSong('Rock')} checked={songProvider.selectedSong === "Rock"}/>
+                    Rock
+                </div>
+                <button className="NavButton"  onClick={()=>songProvider.setSelectedSong('Song')} >Reset </button>
+            </div>
+        </div>)
+
+
+}
